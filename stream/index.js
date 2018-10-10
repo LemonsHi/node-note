473aa6f413f1691c803af73436fa94e4db9223ec
LemonsHi <wulemons@gmail.com>
2018-10-10 15:41:55 +0800

    'fix'

--- stream/index.js
+++ stream/index.js
@@ -1,16 +1,19 @@
-var fs = require("fs");
-var zlib = require('zlib');
+var fs = require("fs")
+var zlib = require('zlib')
+
+let writerStream = fs.createWriteStream('output.txt')
 
 // 压缩 input.txt 文件为 input.txt.gz
 fs.createReadStream('../input.txt')
   .pipe(zlib.createGzip())
-  .pipe(fs.createWriteStream('input.txt.gz'));
+  .pipe(fs.createWriteStream('input.txt.gz'))
+fs.createReadStream('../input.txt').pipe(writerStream)
 
-console.log("文件压缩完成。");
+console.log("文件压缩以及输出完成。")
 
 // 解压 input.txt.gz 文件为 input.txt
 // fs.createReadStream('input.txt.gz')
 //   .pipe(zlib.createGunzip())
-//   .pipe(fs.createWriteStream('../input.txt'));
+//   .pipe(fs.createWriteStream('../input.txt'))
 //
-// console.log("文件解压完成。");
+// console.log("文件解压完成。")
